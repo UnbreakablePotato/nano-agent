f1a10cf96e1dd767a2ea229e8030222c962f0c93
UnbreakablePotato <swipeytwit@gmail.com>
2026-04-25 20:40:07 +0200

    Fixed a bug, which caused the full path to repeat the file name

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -7,20 +7,22 @@ def get_file_content(working_dir, file_path):
 
     target_dir = os.path.normpath(os.path.join(abs,file_path))
 
-    full_path = os.path.join(target_dir,file_path)
-
     valid_target_dir = os.path.commonpath([abs, target_dir]) == abs
 
+    valid_file = os.path.isfile(target_dir)
+
     if not valid_target_dir:
-        print(f"Error: cannot read \"{file_path}\" as it is outside the permitted working directory")
+        print(f"Error: cannot read \"{target_dir}\" as it is outside the permitted working directory")
         return
     
-    if not os.path.isfile(full_path):
-        print(f"Error: File not found or is not a regular file: \"{file_path}\"")
+    if not valid_file:
+        print(f"Error: File not found or is not a regular file: \"{target_dir}\"")
         return
     
-    with open(file_path, "r") as f:
+    with open(target_dir, "r") as f:
         file_content_string = f.read(MAX_CHARS)
-        if f.read(MAX_CHARS + 1) is not None:
-            content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+        if f.read(MAX_CHARS + 1) != "":
+            file_content_string += f'[...File "{target_dir}" truncated at {MAX_CHARS} characters]'
+    print(file_content_string)
+    return
 
