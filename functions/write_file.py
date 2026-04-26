4f96a4a96752dbbe2462cd26eda20853ce746eb2
UnbreakablePotato <swipeytwit@gmail.com>
2026-04-26 11:30:47 +0200

    Added function, that allows the llm to overwrite files in a specific working directory

---
+++ functions/write_file.py
@@ -0,0 +1,26 @@
+import os
+def write_file(working_dir, file_path, content):
+    abs = os.path.abspath(working_dir)
+
+    target_dir = os.path.normpath(os.path.join(abs,file_path))
+
+    valid_target_dir = os.path.commonpath([abs, target_dir]) == abs
+
+    existing_file = os.path.isfile(target_dir)
+
+    is_dir = os.path.isdir(target_dir)
+
+    if not valid_target_dir:
+        print(f"Error: cannot write to \"{file_path}\" as it is outside the permitted working area")
+        return
+    
+    if is_dir:
+        print(f"Error: Cannot write to \"{file_path}\" as it is a directory")
+        return
+    
+    os.makedirs(working_dir, exist_ok=True)
+
+    with open(target_dir, "w") as f:
+        f.write(content)
+    print(f"Successfully wrote to \"{file_path}\" ({len(content)} characters written)")
+    return 
