c0e8a94759ee51dcad64fdc229dacc61f887e50f
UnbreakablePotato <swipeytwit@gmail.com>
2026-04-27 09:30:00 +0200

    Updated the system prompts to lessen the amount of hallucinations

--- system_prompts.py
+++ system_prompts.py
@@ -3,10 +3,10 @@ You are a helpful AI coding agent.
 
 When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
 
-- List files and directories
-- Read file contents
-- Execute Python files with optional arguments
-- Write or overwrite files
+- List files and directories. The function name for this task is get_files_info
+- Read file contents. The function name for this task is get_file_content
+- Execute Python files with optional arguments. The function name for this task is run_python_files 
+- Write or overwrite files. The functions name for this task is write_file
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
