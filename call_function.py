04aa7d7372e25e04064d6287b64e0f31251820b0
UnbreakablePotato <swipeytwit@gmail.com>
2026-04-30 16:39:07 +0200

    Refactored the call_function

--- call_function.py
+++ call_function.py
@@ -7,23 +7,31 @@ from functions.get_file_content import get_file_content
 from functions.get_files_info import get_files_info
 from functions.write_file import write_file
 from functions.run_python_file import run_python_file
+from functions.create_directory import create_dir, schema_create_dir
 
 available_functions = types.Tool(
-    function_declarations=[schema_get_files_info, schema_get_file_content, schema_run_python_file, schema_write_file],
+    function_declarations=[
+        schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file,
+        schema_create_dir
+    ]
 )
+
+function_map = {
+    "get_file_content": get_file_content,
+    "get_files_info": get_files_info,
+    "write_file": write_file,
+    "run_python_file": run_python_file,
+    "create_dir": create_dir
+    }
 
 def call_function(function_call, verbose=False):
     if verbose is True:
         print(f"Calling function: {function_call.name}({function_call.args})")
     else:
         print(f" - Calling function: {function_call.name}")
-
-    function_map = {
-    "get_file_content": get_file_content,
-    "get_files_info": get_files_info,
-    "write_file": write_file,
-    "run_python_file": run_python_file,
-    }
 
     func_name = function_call.name or ""
 
