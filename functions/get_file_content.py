53887bf71feba4e2dfa275901e36ad55d60e7eaa
UnbreakablePotato <swipeytwit@gmail.com>
2026-04-30 15:45:48 +0200

    Wrapped the function in try except

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -3,29 +3,30 @@ from google.genai import types
 def get_file_content(working_dir, file_path):
 
     MAX_CHARS = 10000
+    try:
+        abs = os.path.abspath(working_dir)
 
-    abs = os.path.abspath(working_dir)
+        target_dir = os.path.normpath(os.path.join(abs,file_path))
 
-    target_dir = os.path.normpath(os.path.join(abs,file_path))
+        valid_target_dir = os.path.commonpath([abs, target_dir]) == abs
 
-    valid_target_dir = os.path.commonpath([abs, target_dir]) == abs
+        valid_file = os.path.isfile(target_dir)
 
-    valid_file = os.path.isfile(target_dir)
-
-    if not valid_target_dir:
-        print(f"Error: cannot read \"{target_dir}\" as it is outside the permitted working directory")
-        return
-    
-    if not valid_file:
-        print(f"Error: File not found or is not a regular file: \"{target_dir}\"")
-        return
-    
-    with open(target_dir, "r") as f:
-        file_content_string = f.read(MAX_CHARS)
-        if f.read(MAX_CHARS + 1) != "":
-            file_content_string += f'[...File "{target_dir}" truncated at {MAX_CHARS} characters]'
-    print(file_content_string)
-    return
+        if not valid_target_dir:
+            print(f"Error: cannot read \"{target_dir}\" as it is outside the permitted working directory")
+            return
+        
+        if not valid_file:
+            print(f"Error: File not found or is not a regular file: \"{target_dir}\"")
+            return
+        
+        with open(target_dir, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            if f.read(MAX_CHARS + 1) != "":
+                file_content_string += f'[...File "{target_dir}" truncated at {MAX_CHARS} characters]'
+        return file_content_string
+    except Exception as e:
+        return f"Error reading file \"{file_path}\": {e}"
 
 schema_get_file_content = types.FunctionDeclaration(
     name="get_file_content.py",
